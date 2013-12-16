6746acf4a367810a50077bac72b8d04395947103
larahy <larahy@gmail.com>
2013-12-16 19:21:14 +0000

    css added

--- friday.rb
+++ friday.rb
@@ -22,8 +22,22 @@ def random_images
 	sixties = Photo.new("/larabarrr/60s.png")
 	croatia = Photo.new("/larabarrr/croatia.png")
 	bike = Photo.new("/larabarrr/biker_babe.png")
+	icecream = Photo.new("/larabarrr/icecream.png")
+	beverly = Photo.new("/larabarrr/beverly.png")
+	birthday = Photo.new("/larabarrr/birthday_stripes.png")
+	walken = Photo.new("/larabarrr/walken_pinstripe.png")
+	water = Photo.new("/larabarrr/water.png")
+	susanna = Photo.new("/larabarrr/susanna.png")
+	stripe = Photo.new("/larabarrr/bluestripes.png")
+	australia = Photo.new("/larabarrr/australia.png")
+	july = Photo.new("/larabarrr/july.png")
+	powder = Photo.new("/larabarrr/powder_room.png")
+	guiness = Photo.new("/larabarrr/guiness.png")
+	hills = Photo.new("/larabarrr/hills.png")
+	luggage = Photo.new("/larabarrr/luggage.png")
+	inukshuk = Photo.new("/larabarrr/inukshuk.png")
 
-	images.insert(0, sailors, aero, attitude, bath, cafe, rainbow, zebra, bikes, jules, bike, croatia, sixties, tennis, picasso, matisse, boat)
+	images.insert(0, sailors, aero, attitude, bath, cafe, rainbow, zebra, bikes, jules, croatia, sixties, tennis, picasso, matisse, icecream, beverly, birthday, walken, water, susanna, stripe, australia, july, powder, guiness, hills, luggage, inukshuk)
 	images
 end
 
