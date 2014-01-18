fa4612e52b86ecb5541c68820415f45ae3bdbd5c
larahy <larahy@gmail.com>
2014-01-18 15:53:05 +0000

    minor style changes

--- lib/photo.rb
+++ lib/photo.rb
@@ -8,18 +8,3 @@ class Photo
   
 end
 
-  # images = Array.new()
-  # sailors = Photo.new("/larabarrr/sailors.png")
-  # aero = Photo.new("/larabarrr/aeronautical.png")
-  # attitude = Photo.new("/larabarrr/attitude.png")
-  # bath = Photo.new("/larabarrr/bath_socks.png")
-  # cafe = Photo.new("/larabarrr/cafe_de_la_marie.png")
-  # rainbow = Photo.new("/larabarrr/rainbow.png")
-  # zebra = Photo.new("/larabarrr/zebra.png")
-  # jules = Photo.new("/larabarrr/jules_et_jim.png")
-  # bikes = Photo.new("/larabarrr/racing_stripes.png")
-
-  # images.insert(0, sailors, aero, attitude, bath, cafe, rainbow, zebra, bikes, jules)
-  # images
-
-  # puts images[2]
